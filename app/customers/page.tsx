ba9aa117ca483d09b6b45c409134197d3de6a780
Hector <torrezhectorb@gmail.com>
2024-02-12 14:46:38 -0600

    feat/ add search bar and fix style in mobile

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -2,23 +2,29 @@
 import { users } from "@/data/data";
 import Navbar from "../../components/Navbar";
 import React from "react";
-import { Avatar } from "@nextui-org/react";
+import { Avatar, Input } from "@nextui-org/react";
 import Dropdown from "@/components/Dropdown";
+import { SearchIcon } from "lucide-react";
 
 export default function page() {
   return (
     <>
       <Navbar />
-      <div className="max-w-[1000px] m-auto flex flex-wrap">
-        <ul className="flex flex-col gap-5 ">
+      <div className="flex justify-center my-10 ">
+        <Input
+          label="Search your client"
+          className="max-w-[300px]"
+          startContent={<SearchIcon size={20} />}
+        />
+      </div>
+      <div className="max-w-[1000px] m-auto flex flex-wrap justify-center">
+        <ul className="flex flex-col gap-5 lg:grid lg:grid-cols-2">
           {users.map((user) => {
             return (
               <li className="flex items-center justify-between  " key={user.id}>
                 <div className="flex gap-3 items-center">
                   <Avatar src={user.avatar} />
-                  <p className="hidden sm:block">{user.name} - </p>
                   <p>{user.email}</p>
-                  <p>- {user.age}</p>
                 </div>
                 <div className="">
                   <Dropdown />
