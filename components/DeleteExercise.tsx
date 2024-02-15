decfd0f5c41392314734ce070eb5e0fea04d4ec1
Hector <torrezhectorb@gmail.com>
2024-02-15 15:16:31 -0600

    feat: add button to remove the current exercise

---
+++ components/DeleteExercise.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
+
+export default function App() {
+  const { isOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <div className="flex flex-wrap gap-3"></div>
+      <Modal size={"xs"} isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Title
+              </ModalHeader>
+
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
