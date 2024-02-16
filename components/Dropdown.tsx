7a90b181e5f73fed353ef03b9d0767a620063dac
Hector <torrezhectorb@gmail.com>
2024-02-16 09:58:17 -0600

    style: add new icon

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -6,18 +6,18 @@ import {
   DropdownItem,
   Button,
 } from "@nextui-org/react";
+import { MoreHorizontal } from "lucide-react";
 
 export default function App() {
   return (
     <Dropdown>
       <DropdownTrigger>
         <Button size="sm" variant="bordered">
-          Edit
+          <MoreHorizontal />
         </Button>
       </DropdownTrigger>
       <DropdownMenu aria-label="Example with disabled actions">
         <DropdownItem key="new">New Routine</DropdownItem>
-        <DropdownItem key="copy">New meal</DropdownItem>
         <DropdownItem key="delete" className="text-danger" color="danger">
           Delete user
         </DropdownItem>
